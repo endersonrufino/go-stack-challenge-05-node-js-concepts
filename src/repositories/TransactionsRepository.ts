0f0b128a52363aff0d6270e65eea4c99b3a2583f
Enderson Rufino Amorim <endersonrufinoamorim@outlook.com>
2020-04-17 08:30:05 -0300

    finalizado rotas

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,18 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransaction {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +23,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    for (const transaction of this.transactions) {
+      if (transaction.type === 'income') {
+        income += transaction.value;
+      }
+
+      if (transaction.type === 'outcome') {
+        outcome += transaction.value;
+      }
+    }
+
+    total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
